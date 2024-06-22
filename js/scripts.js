4742ae444d020f46db8cc1517ab580681bf2297e
LegoOdin <96894245+LegoOdin@users.noreply.github.com>
2024-06-22 19:06:57 +0930

    Button effects changed smooth scrolling with CSS added, hamburger menu added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log('Hello, welcome to my portfolio web site');
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
